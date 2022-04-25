8862231989964e177ba76f00778d75f987b72b09
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-04-25 17:03:34 +0800

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 
 const schedule = require('../controller/schedules_controller');
+const displaySchedule = require('../controller/display_controller');
 
 
 const router = require('express').Router();
@@ -10,6 +11,7 @@ const router = require('express').Router();
 
 ////ALL OFFICIALS FUNCTIONALITIES
 router.post('/submitSchedule',schedule); ///TIME IN  
+router.get('/displaySchedule',displaySchedule); ///TIME IN  
 
 
 
